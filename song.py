7ca662872c4967fee20acaad80ca3763b44a4b0b
Xiuyan <xiuyansyu@gmail.com>
2018-03-15 20:32:55 -0400

    timestamps method
    getTimestamps method of songdatabase made exposed, as well as related functionality; small bug fix: timestamps and melody pitch differentials are now saved as float instead of string

--- song.py
+++ song.py
@@ -7,8 +7,7 @@ Created on Fri Feb 23 14:29:34 2018
 
 from mido import Message, MidiFile, MidiTrack, MetaMessage
 
-#SONG_PATH = "../musicbank"
-SONG_PATH = ""
+SONG_PATH = "musicbank/"
 
 class Song(object):
     """
@@ -20,6 +19,7 @@ class Song(object):
         self.wav = SONG_PATH + self.file.split('.')[0] + ".wav"
         self.melodyfile = self.file.split('.')[0] + "_stripped.mid"
         self.pitch_diff = None
+        self.timestamps = None
         
         mid = MidiFile(self.melodyfile)
         track = mid.tracks[0]
@@ -28,6 +28,10 @@ class Song(object):
                 self.firstnote = msg.note
                 break
         
+    def preprocess(self):
+        self.fetchMelodyPitchDiff()
+        self.fetchTimestamps()
+    
     def setFile(self, file):
         self.file = SONG_PATH + file
     
@@ -38,11 +42,24 @@ class Song(object):
         """
         Outputs list of intervals' semitone difference
         """
-        file = open(self.name + "_stripped.txt", 'r')
+        file = open(SONG_PATH + self.name + "_stripped.txt", 'r')
         pitch_diff_string = file.readline()
-        pitch_diff = pitch_diff_string.split(",")
+        pitch_diff = (pitch_diff_string.strip()).split(",")
         file.close()
+        pitch_diff = [float(i) for i in pitch_diff]
         self.pitch_diff = pitch_diff
+        
+    def fetchTimestamps(self):
+        """
+        Outputs list of intervals' semitone difference
+        """
+        file = open(SONG_PATH + self.name + "_timestamps.txt", 'r')
+        timestamps_string = file.readline()
+        timestamps = (timestamps_string.strip()).split(",")
+        file.close()
+        timestamps = [float(i) for i in timestamps]
+        self.timestamps = timestamps
+            
         
     def getMIDI(self):
         return self.file
@@ -69,20 +86,24 @@ class SongDatabase(object):
     def preprocessMelodies(self):
         
         for songname in self.songs.keys():
-            self.songs[songname].fetchMelodyPitchDiff()
+            self.songs[songname].preprocess()
     
     def getMelody(self, songname):
-        
         return self.songs[songname].pitch_diff
     
     def getFirstNote(self, songname):
         return self.songs[songname].firstnote
     
+    def getTimestamps(self, songname):
+        return self.songs[songname].timestamps
+    
 if __name__ == "__main__":
     
-    allSongNames = ["twinkle","london_bridge","lullaby","mary_had_a_little_lamb","three_blind_mice"]
+    allSongNames = ["twinkle","london_bridge","three_blind_mice"]
+    #allSongNames = ["twinkle","london_bridge","lullaby","mary_had_a_little_lamb","three_blind_mice","spider"]
     
     songdb = SongDatabase(allSongNames)
     songdb.preprocessMelodies()
     
-    melody = songdb.getMelody("twinkle")
+    melody = songdb.getMelody("three_blind_mice")
+    timestamps = songdb.getTimestamps("three_blind_mice")
