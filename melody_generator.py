409308212cdfd98ce156dc263d16b5325d3bf449
lavanova <icebergmatt0109@gmail.com>
2018-03-16 22:50:37 -0400

    integrated db

--- melody_generator.py
+++ melody_generator.py
@@ -4,12 +4,13 @@ import argparse
 import os
 import numpy as np
 from scipy.signal import medfilt
-import __init__
+#import __init__
 import pyaudio
 import sys
 import aubio
 from aubio import sink
 from songmatch import *
+from song import *
 from operator import itemgetter
 from pprint import PrettyPrinter
 pp = PrettyPrinter(indent=4)
@@ -60,13 +61,26 @@ temporatio = None
 startpt = None
 
 # song-matcher setup
-songs = {'Twinkle Twinkle Little Star':[0,7,0,2,0,-2,-2,0,-1,0,-2,0,0,0,-2,7,0,-2,0,-1,0,-2,5,0,-2,0,-1,0,-2,-2,0,7,0,2,0,-2,-2,0,-1,0,-2,0,-2],
-         'Three Blind Mice':[-2,-2,4,-2,-2,7,-2,0,-1,3,-2,0,-1,3,5,0,-1,-2,2,1,-5,0,0,5,0,0,-1,-2,2,1,-5,0,0,5,0,0,-1,-2,2,1,-5,0,0,-2,-1,-2,-2,4,-2,-2,4,-2,-2,7,-2,0,-1,3,-2,0,-1,3,5,0,-1,-2,2,1,-5,0,0,5,0,0,-1,-2,2,1,-5,0,0,5,0,0,-1,-2,2,1,-5,0,0,-2,-1,-2,-2]}
-timestamps = {'Twinkle Twinkle Little Star':[0.49991319444444443, 1.0040273401027078, 1.508141485760971, 2.0122556314192344, 2.516369777077498, 3.020483922735761, 4.0287122140522875, 4.532826359710551, 5.036940505368814, 5.541054651027078, 6.04516879668534, 6.297225869514473, 6.549282942343604, 6.801340015172736, 7.0533970880018675, 7.8641806722689065, 8.565739524976657, 9.06985367063492, 9.573967816293184, 10.078081961951447, 10.582196107609711, 11.086310253267973, 12.0945385445845, 12.598652690242764, 13.102766835901026, 13.606880981559291, 14.110995127217553, 14.615109272875818, 15.11922341853408, 16.127451709850607, 16.63156585550887, 17.135680001167135, 17.6397941468254, 18.14390829248366, 18.648022438141922, 19.152136583800186, 20.160364875116713, 20.664479020774976, 21.16859316643324, 21.672707312091504, 22.176821457749767, 22.68093560340803, 23.18504974906629, 23.995833333333337]}
-start_notes = {'Twinkle Twinkle Little Star': 65}
-wav_files = {'Twinkle Twinkle Little Star':'./musicbank/twinkle.wav',
-             'Three Blind Mice': './musicbank/three_blind_mice.wav'}
+# songs = {'Twinkle Twinkle Little Star':[0,7,0,2,0,-2,-2,0,-1,0,-2,0,0,0,-2,7,0,-2,0,-1,0,-2,5,0,-2,0,-1,0,-2,-2,0,7,0,2,0,-2,-2,0,-1,0,-2,0,-2],
+#          'Three Blind Mice':[-2,-2,4,-2,-2,7,-2,0,-1,3,-2,0,-1,3,5,0,-1,-2,2,1,-5,0,0,5,0,0,-1,-2,2,1,-5,0,0,5,0,0,-1,-2,2,1,-5,0,0,-2,-1,-2,-2,4,-2,-2,4,-2,-2,7,-2,0,-1,3,-2,0,-1,3,5,0,-1,-2,2,1,-5,0,0,5,0,0,-1,-2,2,1,-5,0,0,5,0,0,-1,-2,2,1,-5,0,0,-2,-1,-2,-2]}
+# timestamps = {'Twinkle Twinkle Little Star':[0.49991319444444443, 1.0040273401027078, 1.508141485760971, 2.0122556314192344, 2.516369777077498, 3.020483922735761, 4.0287122140522875, 4.532826359710551, 5.036940505368814, 5.541054651027078, 6.04516879668534, 6.297225869514473, 6.549282942343604, 6.801340015172736, 7.0533970880018675, 7.8641806722689065, 8.565739524976657, 9.06985367063492, 9.573967816293184, 10.078081961951447, 10.582196107609711, 11.086310253267973, 12.0945385445845, 12.598652690242764, 13.102766835901026, 13.606880981559291, 14.110995127217553, 14.615109272875818, 15.11922341853408, 16.127451709850607, 16.63156585550887, 17.135680001167135, 17.6397941468254, 18.14390829248366, 18.648022438141922, 19.152136583800186, 20.160364875116713, 20.664479020774976, 21.16859316643324, 21.672707312091504, 22.176821457749767, 22.68093560340803, 23.18504974906629, 23.995833333333337]}
+# start_notes = {'Twinkle Twinkle Little Star': 65}
+# wav_files = {'Twinkle Twinkle Little Star':'./musicbank/twinkle.wav',
+#              'Three Blind Mice': './musicbank/three_blind_mice.wav'}
+
+# song database
+allSongNames = ["twinkle","london_bridge","three_blind_mice","boat","lullaby","mary_had_a_little_lamb"]
+songdb = SongDatabase(allSongNames)
+songdb.preprocessMelodies()
+
+songs = songdb.getAllMelody()
+start_notes = songdb.getAllFirstNode()
+timestamps = songdb.getAllTimestamps()
+
+# song matcher
 song_matcher = SongsMatchNew(songs, timestamps)
+
+
 
 # initialise pyaudio
 p = pyaudio.PyAudio()
@@ -94,13 +108,13 @@ def process_audio(in_data, frame_count, time_info, status):
     global keydiff
     global temporatio
     global startpt
-    
+
     time_counter += seconds_per_sample
-    
+
     signal = np.fromstring(in_data, dtype=np.float32)
     pitch = pitch_o(signal)[0]
     confidence = pitch_o.get_confidence()
-    
+
     # process pitch information
     if (pitch < thresh_low or pitch > thresh_high) and not start:
         start = True
@@ -117,7 +131,7 @@ def process_audio(in_data, frame_count, time_info, status):
                 pitches[-1].append(pitch)
                 if not durations[-1]: # if start note
                     durations[-1] = [time_counter]
-                    
+
     # if note ends
     if len(pitches) > 2 :
         if not seq: # if seq empty
@@ -132,7 +146,7 @@ def process_audio(in_data, frame_count, time_info, status):
         else:
             seq.append(int(pitches[-2]-pitches[-3]))
         pitches.pop(0)
-        
+
         # add the obtained note to song_matcher to get probability
         song_matcher.addNote([seq[-1]])
         scores = song_matcher.getProbDic()
@@ -151,8 +165,8 @@ def process_audio(in_data, frame_count, time_info, status):
             print("+++++++++++++")
 #            WavPlayer(wav_files[song])
 #            return (in_data, pyaudio.paComplete)
-            
-    
+
+
     g(signal, hop_s)
     return (in_data, pyaudio.paContinue)
 
@@ -173,7 +187,7 @@ while stream.is_active():
         print("*** Ctrl+C pressed, exiting")
         break
 
-print("*** done recording")   
+print("*** done recording")
 stream.stop_stream()
 stream.close()
 p.terminate()
