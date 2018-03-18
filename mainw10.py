d427f0071bc378d737578b698df860609d570cd4
Dami Choi <dami.choi@mail.utoronto.ca>
2018-03-18 13:56:54 -0400

    integrated playing from wav file into mainw10

--- mainw10.py
+++ mainw10.py
@@ -15,6 +15,7 @@ from operator import itemgetter
 from pprint import PrettyPrinter
 pp = PrettyPrinter(indent=4)
 from wavplayer import *
+from scipy.io.wavfile import read
 
 def convert_durations(ds):
     start_time = ds[0][0]
@@ -25,6 +26,7 @@ def convert_durations(ds):
     return durations
 
 # pyaudio params
+wf = read('twinkle.wav')[1]
 buffer_size = 1024
 pyaudio_format = pyaudio.paFloat32
 n_channels = 1
@@ -106,12 +108,18 @@ def process_audio(in_data, frame_count, time_info, status):
     global keydiff
     global temporatio
     global startpt
+    global wf
     global detected
     global matched_song
 
     time_counter += seconds_per_sample
-
-    signal = np.fromstring(in_data, dtype=np.float32)
+    if wf is not None:
+        signal = np.array(wf[:frame_count], dtype=np.float32)
+        if len(signal) < frame_count:
+            return (in_data, pyaudio.paComplete)
+        wf = wf[frame_count:]
+    else:
+        signal = np.fromstring(in_data, dtype=np.float32)
     pitch = pitch_o(signal)[0]
     confidence = pitch_o.get_confidence()
 
