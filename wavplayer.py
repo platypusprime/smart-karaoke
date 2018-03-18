c8943267b7c5867158b86c5daa9c26227b72ca69
Jingchen Xu <joel.jc.xu@gmail.com>
2018-03-17 22:10:30 -0400

    Change wavplayer to use SoundPlus
    Rewrite wavplayer to use SoundPlus as a means to perform real-time
    playback modification. Essentially a wrapper around aupyom and SoundPlus.

--- wavplayer.py
+++ wavplayer.py
@@ -1,17 +1,15 @@
 '''
-Class for playback of .wav files, with adjustments to speed and pitch. Uses
-LibROSA to load wav files and apply audio transformations, and PyAudio to 
-interface with system I/O.
-
-Currently supports only non-real-time operation.
+Class for playback of .wav files, with live adjustments to speed, pitch, and
+volume. Extends aupyom to perform playback modification and uses LibROSA to
+load wav files.
 '''
 
-import pyaudio
-import librosa
+from soundplus import SoundPlus
+from aupyom import Sampler
 
 class WavPlayer(object):
 
-    def __init__(self, file, stretch=1, shift=0):
+    def __init__(self, file, stretch=1, shift=0, offset=0):
         '''
         Creates a player for the specified .wav file with fixed time stretch
         and pitch shift characteristics.
@@ -26,26 +24,51 @@ class WavPlayer(object):
         shift : number, optional
             The pitch shift amount, in semitones. A shift value of 0.0 will
             result in a playback at the original pitch.
+        offset : number, optional
+            The initial frame offset of the playback. An offset value of
+            0 will result in playback from the beginning of the file.
         '''
-        
-        self.y, self.sr = librosa.load(path=file, sr=None, mono=True)
-        self.y_mod = librosa.effects.time_stretch(self.y, stretch)
-        self.y_mod = librosa.effects.pitch_shift(self.y_mod, self.sr, shift)
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(
-                format=pyaudio.paFloat32,
-                channels=1,
-                rate=int(self.sr),
-                output=True)
-        
-        self.stream.start_stream()
-        self.stream.write(self.y_mod, self.y_mod.shape[0])
-        self.stream.stop_stream()
-        self.stream.close()
+
+        self.sampler = Sampler()
+        self.sound = SoundPlus.from_file(file)
+
+    def navigate(self, offset):
+        self.sound.navigate(offset)
+
+    def play(self):
+        self.sampler.play(self.sound)
+
+    def stop(self):
+        self.sound.playing = False
+
+    @property
+    def volume(self):
+        return self.sound.volume
+
+    @volume.setter
+    def volume(self, value):
+        self.sound.volume = value
+
+    @property
+    def pitch_shift(self):
+        return self.sound.pitch_shift
+
+    @pitch_shift.setter
+    def pitch_shift(self, value):
+        self.sound.pitch_shift = value
+
+    @property
+    def time_stretch(self):
+        return self.sound.stretch_factor
+
+    @time_stretch.setter
+    def time_stretch(self, value):
+        self.sound.stretch_factor = value
 
 if __name__ == '__main__':
     # example usage
     proc = WavPlayer("toms_diner.wav")
-    proc = WavPlayer("toms_diner.wav", stretch=1.5)
-    proc = WavPlayer("toms_diner.wav", shift=2)
-    proc = WavPlayer("toms_diner.wav", stretch=0.9, shift=-0.1)
+    proc.play()
+#    proc = WavPlayer("toms_diner.wav", stretch=1.5)
+#    proc = WavPlayer("toms_diner.wav", shift=2)
+#    proc = WavPlayer("toms_diner.wav", stretch=0.9, shift=-0.1)
